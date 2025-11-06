485483e23159f8e478cbc56a8841ace9f8d607a8
Jeremiah Schutte <jere@miah.us>
2025-11-05 20:38:57 -0600

    cleanup

--- src/sql/google-oauth-migration.sql
+++
@@ -1,8 +0,0 @@
--- Migration to add Google OAuth support
--- Add google_id column to users table
-
-ALTER TABLE users ADD COLUMN google_id VARCHAR(255) NULL UNIQUE;
-ALTER TABLE users ADD INDEX idx_google_id (google_id);
-
--- Make password field nullable for OAuth users
-ALTER TABLE users MODIFY COLUMN password VARCHAR(255) NULL;
